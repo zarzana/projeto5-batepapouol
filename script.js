f1593c9a6de7183c9c18f70a637f9b6c2214d988
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-04-16 03:48:14 -0300

    Fixed window reload

--- script.js
+++ script.js
@@ -106,10 +106,18 @@ function sendMessage () {
         const data = {from: username, to: 'Todos', text: userMessage, type: 'message'};
     
         const messagePromisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', data);
-        messagePromisse.then(getMessages);
+        messagePromisse.then(refreshChat);
         messagePromisse.catch(window.location.reload);
 
         document.querySelector('input').value='';
 
     }
 }
+
+function refreshChat () {
+
+    getMessages();
+    document.querySelector('input').value='';
+
+}
+
